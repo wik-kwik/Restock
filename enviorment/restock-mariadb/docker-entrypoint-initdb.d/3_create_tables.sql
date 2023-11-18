2cfc214ca0fa04550f6741479e513256b3b2121e
AMADEUSZ\amade <gunia.amadeusz@gmail.com>
2023-11-18 12:15:23 +0100

    create_tables.sql script updated

--- enviorment/restock-mariadb/docker-entrypoint-initdb.d/3_create_tables.sql
+++ enviorment/restock-mariadb/docker-entrypoint-initdb.d/3_create_tables.sql
@@ -3,18 +3,19 @@ CREATE TABLE restock_db.`users`
     `id`          INT         NOT NULL AUTO_INCREMENT,
     `username`    VARCHAR(30) NOT NULL,
     `password`    TEXT        NOT NULL,
-    `create_date` DATE        NOT NULL,
-    `remove_date` DATE,
+    `create_date` TIMESTAMP   NOT NULL,
+    `modify_date` TIMESTAMP,
+    `remove_date` TIMESTAMP,
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE restock_db.`orders`
 (
     `id`          INT         NOT NULL AUTO_INCREMENT,
-    `create_date` DATE        NOT NULL,
-    `modify_date` DATE,
-    `user_id`     INT         NOT NULL,
     `status`      VARCHAR(20) NOT NULL,
+    `create_date` TIMESTAMP   NOT NULL,
+    `modify_date` TIMESTAMP,
+    `user_id`     INT,
     FOREIGN KEY (user_id) REFERENCES restock_db.`users` (id),
     PRIMARY KEY (`id`)
 );
@@ -24,10 +25,9 @@ CREATE TABLE restock_db.`products`
     `id`          INT         NOT NULL AUTO_INCREMENT,
     `order_id`    INT         NOT NULL,
     `name`        TEXT        NOT NULL,
-    `create_date` DATE        NOT NULL,
     `price`       DOUBLE      NOT NULL,
-    `status`      VARCHAR(20) NOT NULL,
-    `ean_code`    TEXT        NOT NULL,
+    `offer_link`  TEXT        NOT NULL,
+    `create_date` TIMESTAMP   NOT NULL,
     FOREIGN KEY (order_id) REFERENCES restock_db.`orders` (id),
     PRIMARY KEY (`id`)
 );
@@ -38,31 +38,31 @@ CREATE TABLE restock_db.`sensors`
     `name`        VARCHAR(30) NOT NULL,
     `model`       TEXT        NOT NULL,
     `ip_address`  TEXT        NOT NULL,
-    `create_date` DATE        NOT NULL,
-    `modify_date` DATE,
-    `remove_date` DATE,
+    `create_date` TIMESTAMP   NOT NULL,
+    `modify_date` TIMESTAMP,
+    `remove_date` TIMESTAMP,
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE restock_db.`thresholds`
 (
-    `id`          INT    NOT NULL AUTO_INCREMENT,
-    `sensor_id`   INT    NOT NULL,
-    `create_date` DATE   NOT NULL,
-    `modify_date` DATE,
-    `remove_date` DATE,
-    `value`       DOUBLE NOT NULL,
+    `id`          INT         NOT NULL AUTO_INCREMENT,
+    `sensor_id`   INT         NOT NULL,
+    `type`        TEXT        NOT NULL,
+    `value`       DOUBLE      NOT NULL,
+    `create_date` TIMESTAMP   NOT NULL,
+    `modify_date` TIMESTAMP,
+    `remove_date` TIMESTAMP,
     FOREIGN KEY (sensor_id) REFERENCES restock_db.`sensors` (id),
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE restock_db.`sensor_data`
 (
-    `id`          INT  NOT NULL AUTO_INCREMENT,
-    `sensor_id`   INT  NOT NULL,
-    `type`        TEXT NOT NULL,
-    `value`       TEXT NOT NULL,
-    `create_date` DATE NOT NULL,
+    `id`          INT         NOT NULL AUTO_INCREMENT,
+    `sensor_id`   INT         NOT NULL,
+    `value`       TEXT        NOT NULL,
+    `create_date` TIMESTAMP   NOT NULL,
     FOREIGN KEY (sensor_id) REFERENCES restock_db.`sensors` (id),
     PRIMARY KEY (`id`)
 );
