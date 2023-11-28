9eec0e9df815eaac9357ad5222c02f237970182c
j.sopata <jansopata10@gmail.com>
2023-11-28 22:23:01 +0100

    configure cors for login and register

--- restock-backend/src/main/java/com/example/restockbackend/api/AuthApi.java
+++ restock-backend/src/main/java/com/example/restockbackend/api/AuthApi.java
@@ -6,16 +6,13 @@ import com.example.restockbackend.dto.auth.RegisterRequest;
 import com.example.restockbackend.dto.auth.RegisterResponse;
 import com.example.restockbackend.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import javax.naming.AuthenticationException;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthApi {
     private final AuthService authService;
 
