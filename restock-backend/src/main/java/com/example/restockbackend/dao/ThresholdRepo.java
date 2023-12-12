bba6adce594cb056ef977695d92a75130860c177
“Marcin <marcin.karcz374@interia.pl>
2023-12-12 18:43:16 +0100

    query fix

--- restock-backend/src/main/java/com/example/restockbackend/dao/ThresholdRepo.java
+++ restock-backend/src/main/java/com/example/restockbackend/dao/ThresholdRepo.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Repository
 public interface ThresholdRepo extends ListCrudRepository<ThresholdEntity, Long> {
 
-    @Query("SELECT te FROM ThresholdEntity te INNER JOIN SensorEntity se WHERE se.id = te.sensorId and te.type = 'U' AND se.sensorToken = :sensorToken")
+    @Query("SELECT te FROM ThresholdEntity te INNER JOIN SensorEntity se ON se.id = te.sensorId WHERE te.type = 'U' AND se.sensorToken = :sensorToken")
     Optional<ThresholdEntity> getValueBySensorId(String sensorToken);
 
 }
