fe41d5b24fc55465ab68b50e4bd505c5e62920f2
“Marcin <marcin.karcz374@interia.pl>
2023-12-19 18:25:47 +0100

    + mariadb ssl

--- enviorment/restock-mariadb/docker-entrypoint-initdb.d/4_insert_allegro_parameters.sql
+++ enviorment/restock-mariadb/docker-entrypoint-initdb.d/4_insert_allegro_parameters.sql
@@ -1,4 +1,4 @@
-INSERT INTO parameters (id, type, value, create_date, modify_date) VALUES (1, 'S', 'F', CURRENT_TIMESTAMP(), NULL);
-INSERT INTO parameters (id, type, value, create_date, modify_date) VALUES (2, 'B', 'F', CURRENT_TIMESTAMP(), NULL);
-INSERT INTO parameters (id, type, value, create_date, modify_date) VALUES (3, 'Z', 'F', CURRENT_TIMESTAMP(), NULL);
-INSERT INTO parameters (id, type, value, create_date, modify_date) VALUES (4, 'F', 'A', CURRENT_TIMESTAMP(), NULL);
+INSERT INTO restock_db.`parameters` (id, type, value, create_date, modify_date) VALUES (1, 'S', 'F', CURRENT_TIMESTAMP(), NULL);
+INSERT INTO restock_db.`parameters` (id, type, value, create_date, modify_date) VALUES (2, 'B', 'F', CURRENT_TIMESTAMP(), NULL);
+INSERT INTO restock_db.`parameters` (id, type, value, create_date, modify_date) VALUES (3, 'Z', 'F', CURRENT_TIMESTAMP(), NULL);
+INSERT INTO restock_db.`parameters` (id, type, value, create_date, modify_date) VALUES (4, 'F', 'A', CURRENT_TIMESTAMP(), NULL);
