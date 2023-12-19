2af7b3763a6d231a648d559a6318a31d79652e92
j.sopata <jansopata10@gmail.com>
2023-12-19 21:34:54 +0100

    add date to display on orders view

--- restock-backend/src/main/java/com/example/restockbackend/dto/domain/OrderDTO.java
+++ restock-backend/src/main/java/com/example/restockbackend/dto/domain/OrderDTO.java
@@ -1,5 +1,7 @@
 package com.example.restockbackend.dto.domain;
 
+import java.time.LocalDateTime;
+
 public record OrderDTO(Long id,
                        String status,
                        String offerId,
@@ -7,6 +9,7 @@ public record OrderDTO(Long id,
                        String photoUrl,
                        double productPrice,
                        double deliveryPrice,
-                       int smart
+                       int smart,
+                       LocalDateTime createDate
 ) {
 }
