3cc56c39405f4ad36c7aeba9910c4b8b0cd16ba3
wik-kwik <67471556+wik-kwik@users.noreply.github.com>
2023-12-20 01:31:21 +0100

    Design updates

--- restock-frontend/src/components/MainPage/AddDeviceFormElements.js
+++ restock-frontend/src/components/MainPage/AddDeviceFormElements.js
@@ -5,7 +5,8 @@ export const FormWrapper = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  background-color: #afaca7;
+  /* background-color: #afaca7; */
+  background-color: #9ea98f;
   padding: 3em;
   border-radius: 8px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
@@ -23,24 +24,49 @@ export const FormTitle = styled.h2`
 
 export const FormInput = styled.input`
   width: 100%;
-  margin: 10px 0;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+  margin: 0.1em;
+  padding: 0.1em;
+  /* border: 1px solid #ccc;
+  border-radius: 4px; */
+    height: 2em;
+    background: #e9dbde;
+    border: 0.11em solid;
+    border-radius: 0.5em;
+    border-color: grey;
+    color: black;
+    text-align: center;
+    font-size: clamp(1rem, 0.9vw, 1.3rem);
+    
 `;
 
 export const FormButton = styled.button`
-  color: white;
+    /* color: black;
   padding: 10px;
   border: none;
   border-radius: 4px;
   cursor: pointer;
-  background-color: #bdd49d;
+  background-color: #c7aca7;
   margin-top: 1em;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
 
-  &:hover, &:focus {
-        background-color: #a4b888;
-    }
+  &:hover,
+  &:focus {
+    background-color: #d8c1c6;
+  } */
+
+  color: black;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: #c7aca7;
+  margin-top: 1em;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+
+  &:hover,
+  &:focus {
+    background-color: #d8c1c6;
+  }
 `;
 
 export const CloseButton = styled.span`
--- restock-frontend/src/components/MainPage/UserSettingsForm.js
+++ restock-frontend/src/components/MainPage/UserSettingsForm.js
@@ -1,14 +1,17 @@
+// UserSettingsForm.js
 import React, { useState, useEffect } from 'react';
 import {
   FormWrapper,
   FormTitle,
+  FormGroup,
+  FormLabel,
   FormInput,
   FormButton,
   CloseButton,
   SectionLabel,
+  Row
 } from './UserSettingsFormElements';
 import { useAuth } from '../../AuthContext';
-
 
 const UserSettingsForm = ({ onClose, onSubmit, userId }) => {
   const { token } = useAuth();
@@ -36,15 +39,13 @@ const UserSettingsForm = ({ onClose, onSubmit, userId }) => {
         });
         const userData = await response.json();
         setUser(userData);
-        // console.log(userData);
       } catch (error) {
         console.error('Error fetching user data:', error);
       }
     };
-  
+
     fetchUserData();
   }, [token]);
-  
 
   const handleSubmit = async () => {
     try {
@@ -59,21 +60,19 @@ const UserSettingsForm = ({ onClose, onSubmit, userId }) => {
         phoneNumber: user.phoneNumber,
         email: user.email,
       };
-  
-      // Make a POST request to update the user's address
-      const response = await fetch('http://localhost:8080/api/address', {
+
+      const response = await fetch('http://localhost:8080/api/address?id=1', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`, // Assuming 'token' is defined somewhere
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(requestBody),
       });
-  
-      // Check if the request was successful
+
       if (response.ok) {
         console.log('User address updated successfully!');
-        onClose(); // Close the form after successful update
+        onClose();
       } else {
         console.error('Failed to update user address:', response.statusText);
       }
@@ -81,62 +80,107 @@ const UserSettingsForm = ({ onClose, onSubmit, userId }) => {
       console.error('Error updating user address:', error);
     }
   };
-  
 
   return (
     <FormWrapper>
       <CloseButton onClick={onClose}>&times;</CloseButton>
       <FormTitle>User Settings</FormTitle>
 
-      <SectionLabel>Personal Information</SectionLabel>
-      <FormInput
-        type="text"
-        placeholder="First Name"
-        value={user.firstName}
-        onChange={(e) => setUser({ ...user, firstName: e.target.value })}
-      />
-      <FormInput
-        type="text"
-        placeholder="Last Name"
-        value={user.lastName}
-        onChange={(e) => setUser({ ...user, lastName: e.target.value })}
-      />
-      <FormInput
-        type="text"
-        placeholder="Street"
-        value={user.street}
-        onChange={(e) => setUser({ ...user, street: e.target.value })}
-      />
-      <FormInput
-        type="text"
-        placeholder="House Number"
-        value={user.houseNumber}
-        onChange={(e) => setUser({ ...user, houseNumber: e.target.value })}
-      />
-      <FormInput
-        type="text"
-        placeholder="Postal Code"
-        value={user.postalCode}
-        onChange={(e) => setUser({ ...user, postalCode: e.target.value })}
-      />
-      <FormInput
-        type="text"
-        placeholder="City"
-        value={user.city}
-        onChange={(e) => setUser({ ...user, city: e.target.value })}
-      />
-      <FormInput
-        type="text"
-        placeholder="Email"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
-      />
-      <FormInput
-        type="text"
-        placeholder="Phone Number"
-        value={user.phoneNumber}
-        onChange={(e) => setUser({ ...user, phone_number: e.target.value })}
-      />
+      {/* <SectionLabel>Personal Information</SectionLabel> */}
+
+      {/* First Row: First Name and Last Name */}
+      <Row>
+        <FormGroup>
+          <FormLabel htmlFor="firstName">First Name</FormLabel>
+          <FormInput
+            id="firstName"
+            type="text"
+            placeholder="First Name"
+            value={user.firstName}
+            onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+          />
+        </FormGroup>
+        <FormGroup>
+          <FormLabel htmlFor="lastName">Last Name</FormLabel>
+          <FormInput
+            id="lastName"
+            type="text"
+            placeholder="Last Name"
+            value={user.lastName}
+            onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+          />
+        </FormGroup>
+      </Row>
+
+      {/* Second Row: Street */}
+      <FormGroup>
+        <FormLabel htmlFor="street">Street</FormLabel>
+        <FormInput
+          id="street"
+          type="text"
+          placeholder="Street"
+          value={user.street}
+          onChange={(e) => setUser({ ...user, street: e.target.value })}
+        />
+      </FormGroup>
+
+      {/* Third Row: House Number, Postal Code, and City */}
+      <Row>
+        <FormGroup>
+          <FormLabel htmlFor="houseNumber">House Number</FormLabel>
+          <FormInput
+            id="houseNumber"
+            type="text"
+            placeholder="House Number"
+            value={user.houseNumber}
+            onChange={(e) => setUser({ ...user, houseNumber: e.target.value })}
+          />
+        </FormGroup>
+        <FormGroup>
+          <FormLabel htmlFor="postalCode">Postal Code</FormLabel>
+          <FormInput
+            id="postalCode"
+            type="text"
+            placeholder="Postal Code"
+            value={user.postalCode}
+            onChange={(e) => setUser({ ...user, postalCode: e.target.value })}
+          />
+        </FormGroup>
+        <FormGroup>
+          <FormLabel htmlFor="city">City</FormLabel>
+          <FormInput
+            id="city"
+            type="text"
+            placeholder="City"
+            value={user.city}
+            onChange={(e) => setUser({ ...user, city: e.target.value })}
+          />
+        </FormGroup>
+      </Row>
+
+      {/* Last Row: Email and Phone Number */}
+      <Row>
+        <FormGroup>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <FormInput
+            id="email"
+            type="text"
+            placeholder="Email"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+        </FormGroup>
+        <FormGroup>
+          <FormLabel htmlFor="phoneNumber">Phone Number</FormLabel>
+          <FormInput
+            id="phoneNumber"
+            type="text"
+            placeholder="Phone Number"
+            value={user.phoneNumber}
+            onChange={(e) => setUser({ ...user, phoneNumber: e.target.value })}
+          />
+        </FormGroup>
+      </Row>
 
       <FormButton onClick={handleSubmit}>Save User Settings</FormButton>
     </FormWrapper>
