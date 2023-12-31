4a3d4f14c52d1dd4c07118f7db1499a101b1f56f
Amadeusz Gunia <gunia.amadeusz@gmail.com>
2023-12-31 19:32:28 +0100

    dynamic sensor registration (without auth)

--- restock-backend/src/main/java/com/example/restockbackend/service/SensorService.java
+++ restock-backend/src/main/java/com/example/restockbackend/service/SensorService.java
@@ -4,18 +4,25 @@ import com.example.restockbackend.dao.SensorRepo;
 import com.example.restockbackend.dao.ThresholdRepo;
 import com.example.restockbackend.dao.entity.SensorEntity;
 import com.example.restockbackend.dao.entity.ThresholdEntity;
+import com.example.restockbackend.dto.auth.NewSensorRequest;
+import com.example.restockbackend.dto.auth.NewSensorResponse;
 import com.example.restockbackend.dto.domain.SensorDTO;
 import com.example.restockbackend.dto.mapper.SensorMapper;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @AllArgsConstructor
 public class SensorService {
+
+    private final int TOKEN_LENGTH = 25;
+    private final String NEW_SENSOR_NAME = "NEW SENSOR";
 
     private final SensorRepo sensorRepo;
     private final SensorMapper sensorMapper;
@@ -35,6 +42,25 @@ public class SensorService {
                 .collect(Collectors.toList());
     }
 
+    public NewSensorResponse register(NewSensorRequest newSensor) {
+        Optional<SensorEntity> sensorOpt = sensorRepo.findByMacAddress(newSensor.mac());
+        if (sensorOpt.isPresent()) {
+            return new NewSensorResponse(sensorOpt.get().getSensorToken());
+        } else {
+            SensorEntity sensorEntity = new SensorEntity();
+            sensorEntity.setMacAddress(newSensor.mac());
+            sensorEntity.setType(newSensor.type());
+            sensorEntity.setName(NEW_SENSOR_NAME);
+            sensorEntity.setSensorToken(generateRandomString());
+            sensorEntity.setCreateDate(LocalDateTime.now());
+
+            Long newSensorId = sensorRepo.save(sensorEntity).getId();
+            createThresholdsForSensor(newSensorId, newSensor.type());
+
+            return new NewSensorResponse(sensorEntity.getSensorToken());
+        }
+    }
+
     public SensorDTO save(SensorDTO sensor) {
         SensorEntity sensorEntity = sensorMapper.fromDto(sensor);
         sensorEntity.setCreateDate(LocalDateTime.now());
@@ -49,6 +75,25 @@ public class SensorService {
             sensorRepo.save(deletedSensor);
             deleteRelatedThresholds(deletedSensor.getId());
         }
+    }
+
+    private String generateRandomString() {
+        return RandomStringUtils.random(TOKEN_LENGTH, true, true);
+    }
+
+    private void createThresholdsForSensor(Long sensorId, String sensorType) {
+        Stream.of(ThresholdEntity.ThresholdType.UPDATE, ThresholdEntity.ThresholdType.ORDER).forEach(type -> {
+            ThresholdEntity thresholdEntity = new ThresholdEntity();
+            thresholdEntity.setSensorId(sensorId);
+            thresholdEntity.setType(type);
+            thresholdEntity.setCreateDate(LocalDateTime.now());
+
+            if (SensorEntity.SensorType.PHYSICAL_BUTTON.equals(sensorType)) {
+                thresholdEntity.setValue(1);
+            }
+
+            thresholdRepo.save(thresholdEntity);
+        });
     }
 
     private void deleteRelatedThresholds(Long sensorId) {
