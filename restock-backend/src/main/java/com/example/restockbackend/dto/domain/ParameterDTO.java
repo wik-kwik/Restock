5f8a6d498d169985b10544adba43549d5e805bc6
j.sopata <jansopata10@gmail.com>
2024-01-02 15:24:08 +0100

    api for update and get parameters

--- restock-backend/src/main/java/com/example/restockbackend/dto/domain/ParameterDTO.java
+++ restock-backend/src/main/java/com/example/restockbackend/dto/domain/ParameterDTO.java
@@ -4,4 +4,11 @@ package com.example.restockbackend.dto.domain;
 public record ParameterDTO(Long id,
                            String type,
                            String value) {
+    public String getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
