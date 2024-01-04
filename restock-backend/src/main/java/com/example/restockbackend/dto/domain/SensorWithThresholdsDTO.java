a1562e4a8435f08a2561ca3148c2dafe099a7eb6
Amadeusz Gunia <gunia.amadeusz@gmail.com>
2024-01-04 19:01:39 +0100

    unused DTO removed

--- restock-backend/src/main/java/com/example/restockbackend/dto/domain/SensorWithThresholdsDTO.java
+++
@@ -1,5 +0,0 @@
-package com.example.restockbackend.dto.domain;
-
-    public record SensorWithThresholdsDTO(SensorDTO sensor,
-                                      Iterable<ThresholdDTO> thresholds) {
-}
