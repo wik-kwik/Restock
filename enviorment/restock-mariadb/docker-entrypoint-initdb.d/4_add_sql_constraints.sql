154ed0c2868758fce376a0d8abf6a926a1380629
“Marcin <marcin.karcz374@interia.pl>
2024-01-08 20:39:32 +0100

    + sql script typo fix + frontend + backend as a docker service

--- enviorment/restock-mariadb/docker-entrypoint-initdb.d/4_add_sql_constraints.sql
+++ enviorment/restock-mariadb/docker-entrypoint-initdb.d/4_add_sql_constraints.sql
@@ -1,27 +1,27 @@
 -- UNIQUE CONSTRAINTS:
 
-ALTER TABLE users
+ALTER TABLE restock_db.`users`
 ADD CONSTRAINT unique_usernames UNIQUE (username);
 
-ALTER TABLE sensors
+ALTER TABLE restock_db.`sensors`
 ADD CONSTRAINT unique_sensors_by_token UNIQUE (sensor_token);
 
-ALTER TABLE thresholds
+ALTER TABLE restock_db.`thresholds`
 ADD CONSTRAINT unique_thresholds_for_sensor_by_type UNIQUE (sensor_id, type);
 
-ALTER TABLE parameters
+ALTER TABLE restock_db.`parameters`
 ADD CONSTRAINT unique_parameters_by_type UNIQUE (type);
 
 -- ENUM VALIDATION CONSTRAINTS:
 
-ALTER TABLE orders
+ALTER TABLE restock_db.`orders`
 ADD CONSTRAINT check_order_statuses CHECK (status IN ('P', 'A', 'R', 'D', 'C'));
 
-ALTER TABLE sensors
+ALTER TABLE restock_db.`sensors`
 ADD CONSTRAINT check_sensor_types CHECK (type IN ('D', 'B'));
 
-ALTER TABLE thresholds
+ALTER TABLE restock_db.`thresholds`
 ADD CONSTRAINT check_threshold_types CHECK (type IN ('U', 'O'));
 
-ALTER TABLE parameters
+ALTER TABLE restock_db.`parameters`
 ADD CONSTRAINT check_parameters_types CHECK (type IN ('S', 'B', 'Z', 'F'));
